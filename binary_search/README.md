c0bb661d4fb45024c595006fac059e776a5ad49e
Tomoya Tokunaga(徳永朋也) <ttokunag@ucsd.edu>
2019-05-12 15:54:56 -0700

    Updated files via shell

---
+++ binary_search/README.md
@@ -0,0 +1,7 @@
+# Binary Search
+### Tomoya Tokunaga (mailto: ttokunag@ucsd.edu)
+
+This repository is all about binary search.
+I implemented a very basic binary search algorithm in Python and Java codes.
+I also implemented a binary insertion sort, which may be used when we want to insert and sort elements at the same time without linear search.
+
---
+++ binary_search/binary_search_insert.py
@@ -0,0 +1,31 @@
+def bin_search_insert(arr, target):
+    
+    # set min & max index in order to find median index
+    left, right = 0, len(arr)-1
+    # iteratively search where the target fits in
+    while left != right:
+        med = (left + right)//2
+        # the case it finds the exact position
+        if target == arr[med]:
+            arr.insert(med, target)
+            return
+        # if target less than the median, explore the left half
+        elif target < arr[med]:
+            # the case the target is larger than the value right before the median
+            if not med-1 < 0 and target >= arr[med-1]:
+                arr.insert(med, target)
+                return
+            right = med - 1
+        # if target is larger than the median
+        else:
+            if not med+1 > len(arr)-1 and target <= arr[med+1]:
+                arr.insert(med+1, target)
+                return
+            left = med + 1
+    # if the right/left cursor reaches the either ends
+    if left == len(arr)-1:
+        arr.append(target)
+    else:
+        arr.insert(0,target)
+
+    # O(log(n) + n) time | O(1) space
