8492e975cd0f56301e249402e97a35474bec1616
Tomoya Tokunaga <ttokunag@ucsd.edu>
2019-05-24 23:35:16 -0700

    Update binary_search.py

--- BinarySearch/binary_search.py
+++ BinarySearch/binary_search.py
@@ -1,21 +1,27 @@
-def binary_search(arr, target):
+class binary_search(object): 
+    '''
+    * a method to find the given target in the given array. if the
+    * target is found, then return the index of it. Otherwise None
+    * @param arr: an array to be searched
+    * @param target: an element the program looks for
+    '''
+    def find(arr, target):
+        # set min and max index to find a median
+        left, right = 0, len(arr)-1
+        # search an array interatively
+        while left <= right:
+            med = (left + right)//2
+            # the case it finds the exact position
+            if target == arr[med]:
+                return med
+            # if the target is smaller, explore the left half
+            elif target < arr[med]:
+                right = med - 1
+            # if the target is larger, explore the right half
+            else:
+                left = med + 1
 
-    # set min and max index to find a median
-    left, right = 0, len(arr)-1
-    # search an array interatively
-    while left <= right:
-        med = (left + right)//2
-        # the case it finds the exact position
-        if target == arr[med]:
-            return med
-        # if the target is smaller, explore the left half
-        elif target < arr[med]:
-            right = med - 1
-        # if the target is larger, explore the right half
-        else:
-            left = med + 1
-
-    # if the target is not found, return None
-    return None
+        # if the target is not found, return None
+        return None
 
     # O(log(N)) time | O(1) space
