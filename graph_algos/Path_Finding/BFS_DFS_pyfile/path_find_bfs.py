62e3e2858b236477784fb00519ad1002d800f8ab
Tomoya Tokunaga <ttokunag@ucsd.edu>
2019-05-25 00:01:38 -0700

    Update path_find_bfs.py

--- graph_algos/Path_Finding/BFS_DFS_pyfile/path_find_bfs.py
+++ graph_algos/Path_Finding/BFS_DFS_pyfile/path_find_bfs.py
@@ -1,25 +1,74 @@
-# returns True if the path exists
-def path_find_bfs(grid):
+'''
+* Author: Tomoya Tokunaga(mailto: ttokunag@ucsd.edu)
+'''
 
-    visited = []
-    queue = [(0,0)]
+class path_find(object):
+    '''
+    * a method which search a matrix structure to see if there exists
+    * a path between the given two points in a matrix using BFS. returns 
+    * True if there exists a path
+    * @param grid: a matrix to be searched
+    * @param start: start coordinate in the grid
+    * @param dest: destination coordinate in the grid
+    '''
+    def bfs(grid, start=(0,0), dest=(len(grid)-1, len(grid[0])-1)):
+        # nodes in visited are prevented from being re-visited
+        visited = []
+        queue = [start]
 
-    while queue:
+        while queue:
+            node = queue.pop(0)
+            # if a node unvisited search horizontal & vertical direction
+            if node not in visited:
+                if node[0]-1 >= 0 and grid[node[0]-1][node[1]] == 1:
+                    queue.append((node[0]-1, node[1]))
+                if node[0]+1 < len(grid) and grid[node[0]+1][node[1]] == 1:
+                    queue.append((node[0]+1, node[1]))
+                if node[1]-1 >= 0 and grid[node[0]][node[1]-1] == 1:
+                    queue.append((node[0], node[1]-1))
+                if node[1]+1 < len(grid[0]) and grid[node[0]][node[1]+1] == 1:
+                    queue.append((node[0], node[1]+1))
+                # the case reaching the destination coordinate
+                if dest in queue:
+                    return True
+           visited.append(node)
         
-        node = queue.pop(0)
-        if node not in visited:
-            if node[0]-1 >= 0 and grid[node[0]-1][node[1]] == 1:
-                queue.append((node[0]-1, node[1]))
-            if node[0]+1 < len(grid) and grid[node[0]+1][node[1]] == 1:
-                queue.append((node[0]+1, node[1]))
-            if node[1]-1 >= 0 and grid[node[0]][node[1]-1] == 1:
-                queue.append((node[0], node[1]-1))
-            if node[1]+1 < len(grid[0]) and grid[node[0]][node[1]+1] == 1:
-                queue.append((node[0], node[1]+1))
-            # destination coordinate
-            if (len(grid)-1, len(grid[0])-1) in queue:
-                return True
-       
-       visited.append(node)
+        return False
+
     
-    return False
+    '''
+    * a method which search a matrix structure to see if there exists
+    * a path between the given two points in a matrix using DFS. returns 
+    * True if there exists a path
+    * @param grid: a matrix to be searched
+    * @param start: start coordinate in the grid
+    * @param dest: destination coordinate in the grid
+    '''
+    def dfs(grid, start=(0,0), dest=(len(grid)-1, len(grid[0])-1)):
+        # nodes in visited are prevented from being re-visited
+        visited = []
+        stack = [start]
+
+        while stack:
+            node = stack.pop()
+            if node not in visited:
+                if node[0]-1 >= 0 and grid[node[0]-1][node[1]] == 1:
+                    stack.append((node[0]-1, node[1]))
+                if node[0]+1 < len(grid) and grid[node[0]+1][node[1]] == 1:
+                    stack.append((node[0]+1, node[1]))
+                if node[1]-1 >= 0 and grid[node[0]][node[1]-1] == 1:
+                    stack.append((node[0], node[1]-1))
+                if node[1]+1 < len(grid[0]) and grid[node[0]][node[1]+1] == 1:
+                    stack.append((node[0], node[1]+1))
+                # the case reaching the destination coordinate
+                if dest in stack:
+                    return True
+
+            visited.append(node)
+
+        return False
+    
+    
+    
+    
+    
